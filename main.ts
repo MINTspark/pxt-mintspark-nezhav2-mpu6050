f6e062a4a22938b7100b6dcd07f80f4b173fddd8
mpoelzl <mpoelzl@redeaglesolutions.com>
2024-03-09 19:31:53 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace mintspark {
      * NeZha
      */
 
-    //% weight=88
+    //% weight=100
     //% block="Set motor %motor speed to %speed\\%"
     //% group="Motor"
     //% speed.min=-100 speed.max=100
@@ -13,14 +13,14 @@ namespace mintspark {
         neZha.setMotorSpeed(motor, speed);
     }
 
-    //% weight=86
+    //% weight=95
     //% block="Stop motor %motor"
     //% color=#E63022
     export function stopMotor(motor: neZha.MotorList): void {
         setMotorSpeed(motor, 0)
     }
 
-    //% weight=85
+    //% weight=90
     //% block="Stop all motor"
     //% color=#E63022
     export function stopAllMotor(): void {
@@ -30,7 +30,7 @@ namespace mintspark {
         setMotorSpeed(neZha.MotorList.M4, 0)
     }
 
-    //% weight=84
+    //% weight=85
     //% group="Servo"
     //% block="Set servo %servo angle to %angle°"
     //% color=#a3a3c2
@@ -42,56 +42,7 @@ namespace mintspark {
      * PlanetX Sensors
      */
 
-    //% block="Ultrasonic sensor %Rjpin distance %distance_unit"
-    //% group="Sensor"
-    //% Rjpin.fieldEditor="gridpicker"
-    //% Rjpin.fieldOptions.columns=2
-    //% distance_unit.fieldEditor="gridpicker"
-    //% distance_unit.fieldOptions.columns=2
-    //% color=#EA5532
-    export function ultrasoundSensor(Rjpin: PlanetX_Basic.DigitalRJPin, distance_unit: PlanetX_Basic.Distance_Unit_List): number {
-        return PlanetX_Basic.ultrasoundSensor(Rjpin, distance_unit);
-    }
-    
-    //% Rjpin.fieldEditor="gridpicker"
-    //% Rjpin.fieldOptions.columns=2
-    //% group="Sensor"
-    //% color=#EA5532
-    //% block="Line-tracking sensor %Rjpin is %state"
-    export function trackingSensor(Rjpin: PlanetX_Basic.DigitalRJPin, state: PlanetX_Basic.TrackingStateType): boolean {
-        return PlanetX_Basic.trackingSensor(Rjpin, state);
-    }
-
-    //% block="Trimpot %Rjpin analog value"
-    //% Rjpin.fieldEditor="gridpicker"
-    //% Rjpin.fieldOptions.columns=2
-    //% color=#ffcc66 group="Sensor"
-    export function trimpot(Rjpin: PlanetX_Display.AnalogRJPin): number {
-        return PlanetX_Basic.trimpot(Rjpin);
-    }
-
-    //% block="Soil moisture sensor %Rjpin value(0~100)"
-    //% Rjpin.fieldEditor="gridpicker"
-    //% Rjpin.fieldOptions.columns=2
-    //% color=#EA5532 group="Sensor"
-    export function soilHumidity(Rjpin: PlanetX_Basic.AnalogRJPin): number {
-        return PlanetX_Basic.soilHumidity(Rjpin);
-    }
-
-    //% block="Color sensor IIC port color HUE(0~360)"
-    //% group="Sensor" color=#00B1ED
-
-    //%export function readColor(): number {
-    //%    return PlanetX_Basic.readColor();
-    //%}
-
-    //% block="Color sensor IIC port detects %color"
-    //% group="Sensor" color=#00B1ED
-    //% color.fieldEditor="gridpicker" color.fieldOptions.columns=3
-    export function checkColor(color:  PlanetX_Basic.ColorList): boolean {
-        return  PlanetX_Basic.checkColor(color);
-    }
-
+    //% weight=100
     //% block="Crash Sensor %Rjpin is pressed"
     //% Rjpin.fieldEditor="gridpicker"
     //% Rjpin.fieldOptions.columns=2
@@ -100,21 +51,8 @@ namespace mintspark {
         return PlanetX_Basic.Crash(Rjpin);
     }
 
-    /*
-     * PlanetX Output
-     */
-
-    //% block="LED %Rjpin toggle to $ledstate || brightness %brightness \\%"
-    //% Rjpin.fieldEditor="gridpicker" Rjpin.fieldOptions.columns=2
-    //% brightness.min=0 brightness.max=100
-    //% ledstate.shadow="toggleOnOff"
-    //% group="Output" color=#EA5532 
-    //% expandableArgumentMode="toggle"
-    export function ledBrightness(Rjpin: PlanetX_Display.DigitalRJPin, ledstate: boolean, brightness: number = 100): void {
-        PlanetX_Display.ledBrightness(Rjpin, ledstate, brightness);
-    }
-     
     const crashSensorEventId = 54119;
+    //% weight=95
     //% block="Crash Sensor %Rjpin pressed"
     //% Rjpin.fieldEditor="gridpicker"
     //% Rjpin.fieldOptions.columns=2
@@ -127,7 +65,7 @@ namespace mintspark {
                 let isPressed = PlanetX_Basic.Crash(Rjpin);
 
                 if (isPressed && !lastState) {
-                    
+
                     control.raiseEvent(crashSensorEventId, 0);
                 }
                 lastState = isPressed;
@@ -136,6 +74,22 @@ namespace mintspark {
         })
     }
 
+    //% weight=90
+    //% block="Color sensor IIC port detects %color"
+    //% group="Sensor" color=#00B1ED
+    //% color.fieldEditor="gridpicker" color.fieldOptions.columns=3
+    export function checkColor(color: PlanetX_Basic.ColorList): boolean {
+        return PlanetX_Basic.checkColor(color);
+    }
+
+    //% weight=87
+    //% block="Color sensor IIC port color HUE(0~360)"
+    //% group="Sensor" color=#00B1ED
+    //%export function readColor(): number {
+    //%    return PlanetX_Basic.readColor();
+    //%}
+
+    //% weight=85
     const colorSensorEventId = 54120;
     //% block="Color sensor detects %color"
     //% group="Sensor" color=#00B1ED
@@ -154,5 +108,59 @@ namespace mintspark {
                 basic.pause(200);
             }
         })
+    }
+
+    //% weight=80
+    //% block="Ultrasonic sensor %Rjpin distance %distance_unit"
+    //% group="Sensor"
+    //% Rjpin.fieldEditor="gridpicker"
+    //% Rjpin.fieldOptions.columns=2
+    //% distance_unit.fieldEditor="gridpicker"
+    //% distance_unit.fieldOptions.columns=2
+    //% color=#EA5532
+    export function ultrasoundSensor(Rjpin: PlanetX_Basic.DigitalRJPin, distance_unit: PlanetX_Basic.Distance_Unit_List): number {
+        return PlanetX_Basic.ultrasoundSensor(Rjpin, distance_unit);
+    }
+    
+    //% weight=75
+    //% Rjpin.fieldEditor="gridpicker"
+    //% Rjpin.fieldOptions.columns=2
+    //% group="Sensor"
+    //% color=#EA5532
+    //% block="Line-tracking sensor %Rjpin is %state"
+    export function trackingSensor(Rjpin: PlanetX_Basic.DigitalRJPin, state: PlanetX_Basic.TrackingStateType): boolean {
+        return PlanetX_Basic.trackingSensor(Rjpin, state);
+    }
+
+    //% weight=70
+    //% block="Trimpot %Rjpin analog value"
+    //% Rjpin.fieldEditor="gridpicker"
+    //% Rjpin.fieldOptions.columns=2
+    //% color=#ffcc66 group="Sensor"
+    export function trimpot(Rjpin: PlanetX_Display.AnalogRJPin): number {
+        return PlanetX_Basic.trimpot(Rjpin);
+    }
+
+    //% weight=65
+    //% block="Soil moisture sensor %Rjpin value(0~100)"
+    //% Rjpin.fieldEditor="gridpicker"
+    //% Rjpin.fieldOptions.columns=2
+    //% color=#EA5532 group="Sensor"
+    export function soilHumidity(Rjpin: PlanetX_Basic.AnalogRJPin): number {
+        return PlanetX_Basic.soilHumidity(Rjpin);
+    }
+
+    /*
+     * PlanetX Output
+     */
+
+    //% block="LED %Rjpin toggle to $ledstate || brightness %brightness \\%"
+    //% Rjpin.fieldEditor="gridpicker" Rjpin.fieldOptions.columns=2
+    //% brightness.min=0 brightness.max=100
+    //% ledstate.shadow="toggleOnOff"
+    //% group="Output" color=#EA5532 
+    //% expandableArgumentMode="toggle"
+    export function ledBrightness(Rjpin: PlanetX_Display.DigitalRJPin, ledstate: boolean, brightness: number = 100): void {
+        PlanetX_Display.ledBrightness(Rjpin, ledstate, brightness);
     }
 }
